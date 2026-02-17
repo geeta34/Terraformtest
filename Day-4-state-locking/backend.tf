151dcee8f55a874de235963b15f5d424a4888c38
geeta0822 <geetarora0805@gmail.com>
2026-02-17 16:28:19 +0530

    dynamoDB-locking

--- Day-4-state-locking/backend.tf
+++ Day-4-state-locking/backend.tf
@@ -2,8 +2,9 @@ terraform {
   backend "s3" {
     bucket = "terraform-bucket-state-file-locking-storage"
     key    = "terraform.tfstate"
-    use_lockfile = true #to use s3 native locking
-
+    #use_lockfile = true #to use s3 native locking
     region = "us-east-1"
+    dynamodb_table = "geeta" #any version we can use for dynamodb locking
+    encrypt = true
   }
 }
