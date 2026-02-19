c60010903ae4c2412d5bebda37a7f9e21c7233c0
geeta0822 <geetarora0805@gmail.com>
2026-02-20 01:09:06 +0530

    Day-7

---
+++ Day-7-testing/main.tf
@@ -0,0 +1,6 @@
+module "name" {
+    source = "../Day-7-Module"
+    ami_id = "ami-0532be01f26a3de55" 
+    type = "t3.micro"
+    
+}
---
+++ Day-7-values/main.tf
@@ -0,0 +1,6 @@
+module "name" {
+source = "../Day-7-Module"
+ami_id = var.ami_id 
+type = var.type
+}
+  
